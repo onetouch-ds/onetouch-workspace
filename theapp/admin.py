e164e00b1dcd63850e991615daf6750e5e53001f
sw0610 <enthdnjs11@gmail.com>
2020-08-17 23:26:54 +0900

    admin

--- theapp/admin.py
+++ theapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from theapp.models import make_school_vote,make_college_vote, make_dept_vote, Notice
 
 # Register your models here.
+admin.site.register(make_school_vote)
+admin.site.register(make_college_vote)
+admin.site.register(make_dept_vote)
+admin.site.register(Notice)
--- theapp/models.py
+++ theapp/models.py
@@ -8,17 +8,17 @@ class make_school_vote(models.Model):
     school_content = models.TextField(max_length=2000)
     school_deadline = models.DateTimeField()
     school_c1_name =  models.CharField(max_length=45)
-    school_c2_name =  models.CharField(max_length=45)
-    school_c3_name =  models.CharField(max_length=45)
-    school_c4_name =  models.CharField(max_length=45)
-    school_c5_name =  models.CharField(max_length=45)
     school_c1_ex = models.TextField(max_length=2000)
+    school_c2_name =  models.CharField(max_length=45)
     school_c2_ex = models.TextField(max_length=2000)
+    school_c3_name =  models.CharField(max_length=45)
     school_c3_ex = models.TextField(max_length=2000)
+    school_c4_name =  models.CharField(max_length=45)
     school_c4_ex = models.TextField(max_length=2000)
+    school_c5_name =  models.CharField(max_length=45)
     school_c5_ex = models.TextField(max_length=2000)
 
-        def __str__(self):
+    def __str__(self):
         return self.school_title
 
 class make_college_vote(models.Model):
@@ -27,17 +27,17 @@ class make_college_vote(models.Model):
     college_content = models.TextField(max_length=2000)
     college_deadline = models.DateTimeField()
     college_c1_name =  models.CharField(max_length=45)
-    college_c2_name =  models.CharField(max_length=45)
-    college_c3_name =  models.CharField(max_length=45)
-    college_c4_name =  models.CharField(max_length=45)
-    college_c5_name =  models.CharField(max_length=45)
     college_c1_ex = models.TextField(max_length=2000)
+    college_c2_name =  models.CharField(max_length=45)
     college_c2_ex = models.TextField(max_length=2000)
+    college_c3_name =  models.CharField(max_length=45)
     college_c3_ex = models.TextField(max_length=2000)
+    college_c4_name =  models.CharField(max_length=45)
     college_c4_ex = models.TextField(max_length=2000)
+    college_c5_name =  models.CharField(max_length=45)
     college_c5_ex = models.TextField(max_length=2000)
 
-        def __str__(self):
+    def __str__(self):
         return self.college_title
 
 class make_dept_vote(models.Model):
@@ -46,29 +46,34 @@ class make_dept_vote(models.Model):
     dept_content = models.TextField(max_length=2000)
     dept_deadline = models.DateTimeField()
     dept_c1_name =  models.CharField(max_length=45)
-    dept_c2_name =  models.CharField(max_length=45)
-    dept_c3_name =  models.CharField(max_length=45)
-    dept_c4_name =  models.CharField(max_length=45)
-    dept_c5_name =  models.CharField(max_length=45)
     dept_c1_ex = models.TextField(max_length=2000)
+    dept_c2_name =  models.CharField(max_length=45)
     dept_c2_ex = models.TextField(max_length=2000)
+    dept_c3_name =  models.CharField(max_length=45)
     dept_c3_ex = models.TextField(max_length=2000)
+    dept_c4_name =  models.CharField(max_length=45)
     dept_c4_ex = models.TextField(max_length=2000)
+    dept_c5_name =  models.CharField(max_length=45)
     dept_c5_ex = models.TextField(max_length=2000)
 
-        def __str__(self):
+    def __str__(self):
         return self.dept_title                
 
 
-class notice(models.Model):
+class Notice(models.Model):
     noitce_pk = models.AutoField(primary_key=True)
     notice_title = models.CharField(max_length=45)
-    notice_updateday = models.DateTimeField('date published')
     notice_writer = models.CharField(max_length=45)
-    notice_count = models.IntegerField()
+    notice_updateday = models.DateTimeField('date published')
     notice_content = models.TextField(max_length=2000)
+    notice_count = models.IntegerField(default=0)
+    
 
 
     def __str__(self):
         return self.notice_title
     
+    @property
+    def count(self):
+        self.notice_count = self.notice_count+1
+        self.save()
