31fdeee538da7f4b3b42709fc7cfddc44c98f009
sw0610 <enthdnjs11@gmail.com>
2020-08-20 21:36:54 +0900

    공지

--- config/settings.py
+++ config/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'CONN_MAX_AGE':500,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+
     }
 }
 
