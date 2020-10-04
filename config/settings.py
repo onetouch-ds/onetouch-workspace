88c1f1406ff9886b3ea945937275c14d89ea97f3
HyoJeong-Shin <kyn05298@naver.com>
2020-10-04 16:19:44 +0900

    충돌오류

--- config/settings.py
+++ config/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tp)v$pwn2xk2##ugsap(*z-gdkm*ri57wx5^p(tnr!59nqosj*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','tp)v$pwn2xk2##ugsap(*z-gdkm*ri57wx5^p(tnr!59nqosj*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
